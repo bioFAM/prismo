553781f225c7add8df8b9c7f9fd666679a743bd0
Ilia Kats <ilia-kats@gmx.net>
2024-10-21 16:50:10 +0200

    don't export our logger

--- src/famo/__init__.py
+++ src/famo/__init__.py
@@ -2,10 +2,10 @@ import logging
 
 from .core import CORE, DataOptions, ModelOptions, SmoothOptions, TrainingOptions
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 _h = logging.StreamHandler()
 _h.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
 
-logger.setLevel(logging.INFO)
-logger.addHandler(_h)
+_logger.setLevel(logging.INFO)
+_logger.addHandler(_h)
 del _h

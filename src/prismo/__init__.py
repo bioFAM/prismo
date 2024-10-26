34afdd9753538d0b4d7596a12f254f02e13fc887
Martin Rohbeck <35061428+martinrohbeck@users.noreply.github.com>
2024-10-26 19:13:43 +0200

    Improved logging output style

--- src/prismo/__init__.py
+++ src/prismo/__init__.py
@@ -4,7 +4,7 @@ from .prismo import PRISMO, DataOptions, ModelOptions, SmoothOptions, TrainingOp
 
 _logger = logging.getLogger(__name__)
 _h = logging.StreamHandler()
-_h.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+_h.setFormatter(logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s", "%Y-%m-%d %H:%M:%S"))
 
 _logger.setLevel(logging.INFO)
 _logger.addHandler(_h)

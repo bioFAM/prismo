d2225fd76c544df4083ae3b8cc7986a275ca4772
Martin Rohbeck <35061428+martinrohbeck@users.noreply.github.com>
2024-12-10 14:05:37 +0100

    Fix hyper critical bug

--- src/prismo/__init__.py
+++ src/prismo/__init__.py
@@ -5,9 +5,4 @@ from ._core import *  # noqa F403
 from ._version import __version__, __version_tuple__
 
 _logger = logging.getLogger(__name__)
-_h = logging.StreamHandler()
-_h.setFormatter(logging.Formatter("%(asctime)s | %(name)s | %(levelname)s | %(message)s", "%Y-%m-%d %H:%M:%S"))
-
 _logger.setLevel(logging.INFO)
-_logger.addHandler(_h)
-del _h

4a48504446e756e66747bb330de66fbdc28ee1d7
Ilia Kats <ilia-kats@gmx.net>
2025-03-07 11:49:51 +0100

    remove GMT files from repo and add functionality to download MSIGDB on demand

--- src/prismo/tl/__init__.py
+++ src/prismo/tl/__init__.py
@@ -1,3 +1,4 @@
 from ._downstream import match, test
 from ._gpu import get_free_gpu_idx
+from ._msigdb import get_msigdb_features, list_msigdb_categories, list_msigdb_versions
 from ._synthetic import DataGenerator

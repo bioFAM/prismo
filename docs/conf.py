a9ed1ae0d9a14dc4958c793a43bc597f608baf0e
Ilia Kats <ilia-kats@gmx.net>
2025-03-25 15:16:43 +0100

    attempt to fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, str(HERE / "extensions"))
 #       If this is the case, reinstall it to refresh the metadata
 info = metadata("prismo")
 project_name = info["Name"]
-author = info["Author"]
+author = info["Author-email"]
 copyright = f"{datetime.now():%Y}, {author}."
 version = info["Version"]
 urls = dict(pu.split(", ") for pu in info.get_all("Project-URL"))
